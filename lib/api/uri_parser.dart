0d723e67cb6224d359317a799a4581f70c356eae
Keiferms3 <keiferms3@gmail.com>
2023-01-31 15:17:45 -0500

    misc changes made last meeting

--- lib/api/uri_parser.dart
+++ lib/api/uri_parser.dart
@@ -3,6 +3,7 @@ import 'package:authenticator/model/account.dart';
 class UriParser {
   Account parseOtpUri(String uri) {
     // TODO: Implement parsing logic
+    final parsedUri = Uri.parse(uri);
     return Account(uuid: 'abcd');
   }
 }
